08c679c93cb9ac6eb0633ed3e40a1cb66d1ac03a
Mike H <76085826+mhulse11@users.noreply.github.com>
2021-02-19 10:57:29 -0700

    commit Jquery

---
+++ html/js/index.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  $(".bmc-btn-text").hover(function(){
+    $(this).css("background-color", "#FF5F5F");
+    }, function(){
+    $(this).css("background-color", "#333333");
+  });
+  $(".navitem").hover(function(){
+    $(this).css("background-color", "#FF5F5F");
+    }, function(){
+    $(this).css("background-color", "#333333");
+  });
+});
+
+ 
